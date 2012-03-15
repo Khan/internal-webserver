1049528227da11bc7bc5cbb6ecc694b582edec3d
Craig Silverstein <csilvers@khanacademy.org>
2012-03-14 20:48:30 -0700

    Initial commit of files that live on the internal-webserver ec2 machine.

---
+++ git_mirrors/update_mirrors.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LOGFILE=cron.log
+# Create any new repositories that are needed, and update the ones
+# that aren't new.
+echo >>"$LOGFILE"
+date >>"$LOGFILE"
+
+for repo in `python -c 'import json, urllib; print "\n".join(x["url"] for x in json.loads(urllib.urlopen("https://github.com/api/v2/json/repos/show/Khan").read())["repositories"])'`; do
+   dirname=`basename $repo`.git    # the rest is 'https://github.com/Khan'
+   if [ -d "$dirname" ]; then
+     echo "Running git fetch -q in $dirname" >>"$LOGFILE"
+     ( cd $dirname && git fetch -q )
+   else
+     echo "Running git clone --mirror $repo" >>"$LOGFILE"
+     git clone --mirror "$repo"
+   fi
+done
