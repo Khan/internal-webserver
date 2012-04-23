b0e678b8c69f021037ece80188f026cec1e6acef
Craig Silverstein <csilvers@khanacademy.org>
2012-04-23 12:19:24 -0700

    Add timeouts to make sure git-pulling doesn't hang.

--- git_mirrors/update_mirrors.sh
+++ git_mirrors/update_mirrors.sh
@@ -8,14 +8,14 @@
 echo >>"$LOGFILE"
 date >>"$LOGFILE"
 
-for repo in `$PYTHON -c 'import json, urllib; print "\n".join(x["url"] for x in json.loads(urllib.urlopen("https://github.com/api/v2/json/repos/show/Khan").read())["repositories"])'`; do
+for repo in `timeout 1m $PYTHON -c 'import json, urllib; print "\n".join(x["url"] for x in json.loads(urllib.urlopen("https://github.com/api/v2/json/repos/show/Khan").read())["repositories"])'`; do
    dirname=`basename $repo`.git    # the rest is 'https://github.com/Khan'
    if [ -d "$dirname" ]; then
      echo "Running git fetch -q in $dirname" >>"$LOGFILE"
-     ( cd $dirname && git fetch -q )
+     ( cd $dirname && timeout 1m git fetch -q )
    else
      echo "Running git clone --mirror $repo" >>"$LOGFILE"
-     git clone --mirror "$repo"
+     timeout 5m git clone --mirror "$repo"
      touch $dirname/git-daemon-export-ok
    fi
 done
