f55681439b0b87a03db316776cda53caf932bea5
Craig Silverstein <csilvers@khanacademy.org>
2012-04-23 14:26:18 -0700

    Retry the git commands several times, only emitting text if they fail.
    Without this, I was getting cron-mail for every transient failure.
    Now it has to fail 3 times in a row before I get cron-mail.  The
    cron-mail still only indicates there was a failure, though; we'll have
    to go to th3e machien and look in cron.log to actually figure out what
    the failure was.

--- git_mirrors/update_mirrors.sh
+++ git_mirrors/update_mirrors.sh
@@ -3,6 +3,21 @@
 : ${PYTHON:=python}
 : ${LOGFILE:=cron.log}
 
+$@: command to run
+try_several_times() {
+  for i in 0 1 2; do          # try up to 3 times
+    [ "$i" -gt 0 ] && echo "Retrying (retry #$i)"
+
+    output=`"$@" 2>&1`
+    [ $? = 0 ] && return 0
+
+    echo "FAILED:"
+    echo "$output"
+  done
+  return 1   # we never succeeded in running the command
+}
+
+
 # Create any new repositories that are needed, and update the ones
 # that aren't new.
 echo >>"$LOGFILE"
@@ -12,10 +27,14 @@ for repo in `timeout 1m $PYTHON -c 'import json, urllib; print "\n".join(x["url"
    dirname=`basename $repo`.git    # the rest is 'https://github.com/Khan'
    if [ -d "$dirname" ]; then
      echo "Running git fetch -q in $dirname" >>"$LOGFILE"
-     ( cd $dirname && timeout 1m git fetch -q )
+     ( cd $dirname;
+       try_several_times timeout 1m git fetch -q >>"$LOGFILE" \
+         || echo "Failed to run git fetch -q in $dirname"
+     )
    else
      echo "Running git clone --mirror $repo" >>"$LOGFILE"
-     timeout 5m git clone --mirror "$repo"
+     try_several_times timeout 5m git clone --mirror "$repo" >>"$LOGFILE" \
+       || echo "Failed to run git clone --mirror $repo"
      touch $dirname/git-daemon-export-ok
    fi
 done
