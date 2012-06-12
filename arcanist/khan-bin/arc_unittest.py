f1fba89d87ffa085e8f72aba4b7eec0ba7e7150f
Craig Silverstein <csilvers@khanacademy.org>
2012-06-12 16:59:59 -0700

    Add support to auto-update your .arcrc based on changes to the default .arcrc.
    Summary:
    We add a new default khan-arcrc in khan-bin/khan-arcrc.  Whenever we
    sync the new arcanist repo (once per day), we may also get a new copy
    of the default khan-arcrc.  At that time, we check if the new
    khan-arcrc differs from the one you have installed in ~/.arcrc, and if
    so it updates it so they're consistent.
    
    A new khan/do_not_auto_update field is added to suppress this behavior
    on a field-by-field basis, and I set up a default value in the
    khan-arcrc to provide an example of how to use it.
    
    Add support to auto-update your .arcrc based on changes to the default .arcrc.
    
    We add a new default khan-arcrc in khan-bin/khan-arcrc.  Whenever we
    sync the new arcanist repo (once per day), we may also get a new copy
    of the default khan-arcrc.  At that time, we check if the new
    khan-arcrc differs from the one you have installed in ~/.arcrc, and if
    so it updates it so they're consistent.
    
    A new khan/do_not_auto_update field is added to suppress this behavior
    on a field-by-field basis, and I set up a default value in the
    khan-arcrc to provide an example of how to use it.
    
    Fix a bug in relative-pathname logic, and clean up lint errors.
    
    Fixes based on jamie's codereview feedback.
    
    Main one is we emit changes we make to the .arcrc, so people aren't
    surprised.  We also give instructions on how to turn such changes off.
    
    I also fixed the do_not_auto_update field to include the one field we
    actually definitely do *not* want to be auto-updated: the
    script-name.  That's because where it is depends on where you have
    stable checked out on your path.
    
    Don't set scriptandregex.script at all by default.
    
    We don't know the script location on a given user's machine, so we
    can't set this up reliably.  Having a value that's wrong gives a
    very cryptic error message to the user; having no value at all gives
    an only somewhat cryptic error message.  We'll go with the latter for
    now.
    
    I still leave in an example of what this field might look like, to
    make it easier for folks to fill it in.
    
    Add a bunch more entries to do_not_auto_update, especially itself!
    
    If someone changes do_not_auto_update (adding new fields or whatever),
    we don't want that to be overridden by whatever is in the default
    khan-arcrc file.  So make sure do_not_auto_update is itself in the
    no-auto-update list.
    
    I also put in everything else that it's ok if the user changes after
    we set up the default value -- for instance, the 'mail' alias.
    
    Note that these entries will still get *created* the first time,
    unless the user happens to have created a
    'khan/do_not_auto_update' field that turns that off, but once they are
    created, they won't be changed again, at least by the auto-update
    functionality.
    
    (Note: this change will make it difficult for us to automatically add
    stuff to khan/do_not_auto_update in the future.  We may have to
    special-case the handling of that one day.)
    
    Change the formatting to match what json.dumps() does and remove unneeded line.
    
    The unneeded line is 'linter.scriptandregex.script' in
    khan/do_not_auto_update: we do not currently set that field in the
    default .arcrc, since its value is user-dependent.  So no need to
    not-auto-update it.
    
    Fix whitespace, dunno how that got messed up.
    
    Test Plan:
    additions to arc_unittest, and manual testing on my personal
    .arcrc.
    
    additions to arc_unittest, and manual testing on my personal
    .arcrc.
    
    Reviewers: jlfwong
    
    Reviewed By: jlfwong
    
    Differential Revision: http://phabricator.khanacademy.org/D171

--- arcanist/khan-bin/arc_unittest.py
+++ arcanist/khan-bin/arc_unittest.py
@@ -6,7 +6,12 @@ This tests only the KA-specific features, like support for the --rr flag.
 It mocks out all actual arc communication.
 """
 
+import cStringIO
+import copy
+import json
 import os
+import sys
+import tempfile
 import unittest
 
 # This is tricky since the script is called 'arc', not 'arc.py'.  The
@@ -14,9 +19,15 @@ import unittest
 # because it's easier to stub out '__main__' that way, which we need to
 # do so we don't *run* arc when we 'import' it.  The downside
 # is all of arc's symbols are imported into our namespace.  Oh well.
-arc_contents = file(os.path.join(os.path.dirname(__file__), 'arc')).read()
+g_arc_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+arc_contents = open(os.path.join(g_arc_root, 'khan-bin', 'arc')).read()
 arc_contents = arc_contents.replace('__main__', '__not_main__')
 exec(arc_contents)
+# These are just to quiet lint.  They also document what we imported
+_get_user_info = _get_user_info              # @Nolint
+normalize_usernames = normalize_usernames    # @Nolint
+normalize_rr_flags = normalize_rr_flags      # @Nolint
+_update_arcrc = _update_arcrc                # @Nolint
 
 
 def FakeConduitCall(arc_root, conduit_name, json_input={}):
@@ -24,64 +35,64 @@ def FakeConduitCall(arc_root, conduit_name, json_input={}):
     if conduit_name == 'user.query':
         return [
             {
-                "phid"     : "PHID-USER-00000000000000000000",
-                "userName" : "csilvers",
-                "realName" : "Craig Silverstein",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/csilvers\/",
-                "roles"    : [
+                "phid": "PHID-USER-00000000000000000000",
+                "userName": "csilvers",
+                "realName": "Craig Silverstein",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/csilvers\/",
+                "roles": [
                   "admin"
                   ],
                 },
             {
-                "phid"     : "PHID-USER-11111111111111111111",
-                "userName" : "ben",
-                "realName" : "Ben Bentastick",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/ben\/",
-                "roles"    : [],
+                "phid": "PHID-USER-11111111111111111111",
+                "userName": "ben",
+                "realName": "Ben Bentastick",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/ben\/",
+                "roles": [],
                 },
             {
-                "phid"     : "PHID-USER-22222222222222222222",
-                "userName" : "echo",
-                "realName" : "Ben Echoman",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/echo\/",
-                "roles"    : [],
+                "phid": "PHID-USER-22222222222222222222",
+                "userName": "echo",
+                "realName": "Ben Echoman",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/echo\/",
+                "roles": [],
                 },
             {
-                "phid"     : "PHID-USER-33333333333333333333",
-                "userName" : "toom",
-                "realName" : "Toomany Bens",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/toom\/",
-                "roles"    : [],
+                "phid": "PHID-USER-33333333333333333333",
+                "userName": "toom",
+                "realName": "Toomany Bens",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/toom\/",
+                "roles": [],
                 },
             {
-                "phid"     : "PHID-USER-44444444444444444444",
-                "userName" : "Upper",
-                "realName" : "Uppercase Username",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/Upper\/",
-                "roles"    : [],
+                "phid": "PHID-USER-44444444444444444444",
+                "userName": "Upper",
+                "realName": "Uppercase Username",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/Upper\/",
+                "roles": [],
                 },
             {
-                "phid"     : "PHID-USER-55555555555555555555",
-                "userName" : "admin1",
-                "realName" : "Enabled Admin",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/admin1\/",
-                "roles"    : [
+                "phid": "PHID-USER-55555555555555555555",
+                "userName": "admin1",
+                "realName": "Enabled Admin",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/admin1\/",
+                "roles": [
                   "admin",
                   ],
                 },
             {
-                "phid"     : "PHID-USER-66666666666666666666",
-                "userName" : "admin2",
-                "realName" : "Disabled Admin",
-                "image"    : "",
-                "uri"      : "https:\/\/example.com\/p\/admin2\/",
-                "roles"    : [
+                "phid": "PHID-USER-66666666666666666666",
+                "userName": "admin2",
+                "realName": "Disabled Admin",
+                "image": "",
+                "uri": "https:\/\/example.com\/p\/admin2\/",
+                "roles": [
                   "admin",
                   "disabled"
                   ],
@@ -91,7 +102,6 @@ def FakeConduitCall(arc_root, conduit_name, json_input={}):
 
 
 _conduit_call = FakeConduitCall
-
 
 # We can't test pick_a_number, since it has raw_input.  We just always
 # pick the largest numbers.
@@ -227,6 +237,175 @@ class NormalizeRRFlagsTestCase(unittest.TestCase):
         self.assertEqual(['diff', '--reviewers', 'csilvers,echo,toom',
                           '--verbatim', '--dry_run'],
                          actual)
+
+
+class UpdateArcrcTest(unittest.TestCase):
+    def setUp(self):
+        self.start_json = {'a': {'b': {'c': [1, 2, 3]}, 'd': 'hello'},
+                           'b': 'top-level b',
+                           'stays': {'under': 'stays'},
+                           'some': {'stay': 'immutable', 'rest': 'mutable'},
+                           'list': ['top-level list', 4, 5, {'m': 1}],
+                           'khan': {'do_not_auto_update':
+                                    ['stays', 'some/stay']},
+                           }
+        (fd, filename1) = tempfile.mkstemp(prefix='arc_unittest_arcrc')
+        os.write(fd, json.dumps(self.start_json))
+        os.close(fd)
+        self.arcrc = filename1
+
+        (fd, filename2) = tempfile.mkstemp(prefix='arc_unittest_khan_arcrc')
+        os.close(fd)
+        self.default_arcrc = filename2
+
+        # We want to test the stderr output as well.
+        self.old_stderr = sys.stderr
+        sys.stderr = cStringIO.StringIO()
+
+    def tearDown(self):
+        sys.stderr = self.old_stderr
+        if os.path.exists(self.arcrc):
+            os.unlink(self.arcrc)
+        if os.path.exists(self.default_arcrc):
+            os.unlink(self.default_arcrc)
+
+    def _update_with_this_default(self, default_dict):
+        """Write the dict as json to the .arcrc, and calls update_arcrc."""
+        f = open(self.default_arcrc, 'w')
+        json.dump(default_dict, f)
+        f.close()
+        _update_arcrc(g_arc_root,
+                      user_arcrc_file=self.arcrc,
+                      default_arcrc_file=self.default_arcrc,
+                      create_bak=False)
+        return json.load(open(self.arcrc))
+
+    def _arcrc_was_updated(self):
+        """Return true if the test modified the .arcrc file, false else."""
+        # We can tell because when we write the arcrc file in
+        # _update_dict, there's no indent= argument, so it's all on
+        # one line.  But _update_arcrc uses indent=, so it's on many
+        # lines.  If the file is now on many lines, _update_arcrc did
+        # some updating.
+        content = open(self.arcrc).read()
+        return content.count('\n') > 1
+
+    def assert_in_stderr(self, *args):
+        """Assert that all arg occurred somewhere in the stderr output."""
+        stderr_output = sys.stderr.getvalue()
+        for s in args:
+            self.assertTrue(s in stderr_output, (s, stderr_output))
+
+    def assert_stderr_is_empty(self):
+        stderr_output = sys.stderr.getvalue()
+        self.assertFalse(stderr_output)   # should be no output
+
+    def test_update_nonexistent_file(self):
+        os.unlink(self.arcrc)
+        self._update_with_this_default({'a': 1, 'b': 2})
+
+        expected = {'a': 1, 'b': 2}
+        actual = json.load(open(self.arcrc))
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':a ', '  WAS: <empty>\n  NOW: 1\n')
+        self.assert_in_stderr(':b ', '  WAS: <empty>\n  NOW: 2\n')
+
+    def test_update_with_same_default(self):
+        """When the update is the same as the original, it's a noop."""
+        expected = self.start_json
+        actual = self._update_with_this_default(self.start_json)
+        self.assertEqual(expected, actual)
+        self.assertFalse(self._arcrc_was_updated())
+        self.assert_stderr_is_empty()
+
+    def test_simple_addition(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['new'] = 'all new'
+        actual = self._update_with_this_default({'new': 'all new'})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':new ', '  WAS: <empty>\n  NOW: all new\n')
+
+    def test_simple_change(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['b'] = 'changed'
+        actual = self._update_with_this_default({'b': 'changed'})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':b ', '  WAS: top-level b\n  NOW: changed\n')
+
+    def test_deep_addition(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['a']['newb'] = 'new'
+        actual = self._update_with_this_default({'a': {'newb': 'new'}})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':a/newb ', '  WAS: <empty>\n  NOW: new\n')
+
+    def test_deep_change(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['a']['b'] = 'new'
+        actual = self._update_with_this_default({'a': {'b': 'new'}})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':a/b ',
+                              "  WAS: {u'c': [1, 2, 3]}\n  NOW: new\n")
+
+    def test_dict_to_notdict(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['a'] = 'no longer a dict'
+        actual = self._update_with_this_default({'a': 'no longer a dict'})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        old_a = "{u'b': {u'c': [1, 2, 3]}, u'd': u'hello'}"
+        self.assert_in_stderr(':a ',
+                              '  WAS: %s\n  NOW: no longer a dict\n' % old_a)
+
+    def test_notdict_to_dict(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['a']['b']['c'] = {'1': 2, '3': 4}
+        actual = self._update_with_this_default(
+            {'a': {'b': {'c': {'1': 2, '3': 4}}}})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':a/b/c ',
+                              "  WAS: [1, 2, 3]\n  NOW: {u'1': 2, u'3': 4}\n")
+
+    def test_nochange(self):
+        expected = copy.deepcopy(self.start_json)
+        actual = self._update_with_this_default({'b': 'top-level b'})
+        self.assertEqual(expected, actual)
+        self.assertFalse(self._arcrc_was_updated())
+        self.assert_stderr_is_empty()
+
+    def test_no_auto_update(self):
+        expected = self.start_json
+        actual = self._update_with_this_default({'stays': 'changes'})
+        self.assertEqual(expected, actual)
+        self.assertFalse(self._arcrc_was_updated())
+        self.assert_stderr_is_empty()
+
+    def test_nested_no_auto_update(self):
+        expected = self.start_json
+        actual = self._update_with_this_default({'some': {'stay': 'changes'}})
+        self.assertEqual(expected, actual)
+        self.assertFalse(self._arcrc_was_updated())
+        self.assert_stderr_is_empty()
+
+    def test_nested_mix_auto_update_and_not(self):
+        expected = copy.deepcopy(self.start_json)
+        expected['some']['rest'] = 'change'
+        expected['some']['new'] = 'hi'
+        actual = self._update_with_this_default({'some': {'stay': 'changes',
+                                                          'rest': 'change',
+                                                          'new': 'hi'}})
+        self.assertEqual(expected, actual)
+        self.assertTrue(self._arcrc_was_updated())
+        self.assert_in_stderr(':some/rest ',
+                              '  WAS: mutable\n  NOW: change\n')
+        self.assert_in_stderr(':some/new ',
+                              '  WAS: <empty>\n  NOW: hi\n')
 
 
 if __name__ == '__main__':
