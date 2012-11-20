b79c489eec5cbb2c815ad7d09af898c7b5997e70
Craig Silverstein <csilvers@khanacademy.org>
2012-11-20 14:09:46 -0800

    Script to update phabricator on production.
    Summary:
    Updates the internal-webserver repository, which has the phabricator
    source as 'fake sub-repos', and then pushes it to the phabricator
    machine (toby), where it safely restarts the webserver there.
    
    Script to update phabricator on production.
    
    Updates the internal-webserver repository, which has the phabricator
    source as 'fake sub-repos', and then pushes it to the phabricator
    machine (toby), where it safely restarts the webserver there.
    
    Oops, forgot to do the 'git pull' on production!
    
    Check that the 'fake' sub-repos have been set up properly before running.
    
    We could set them up for you if they're not, but since that's a
    destructive operation, I think it's best to let the person run the
    command themselves.  They only have to do it once.
    
    Test Plan:
    Ran 'bash -x update_phabricator.sh' and watched the output.  In
    particular, watched that it properly restarted the daemons on the
    production machine.
    
    Ran 'bash -x update_phabricator.sh' and watched the output.  In
    particular, watched that it properly restarted the daemons on the
    production machine.
    
    crossed fingers
    
    Reviewers: benkomalo
    
    Reviewed By: benkomalo
    
    Differential Revision: http://phabricator.khanacademy.org/D940

---
+++ update_phabricator.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Updates the internal-webserver repository, which has the phabricator
+# source as 'fake sub-repos', and then pushes it to the phabricator
+# machine (toby), where it safely restarts the webserver there.
+
+# Die if something goes wrong.
+set -e
+
+# We need the internal-webserver repository to be set up properly.
+if [ ! -d "phabricator/.git" ]; then
+  echo "You need to set up the phabricator subdirectories as 'fake' sub-repos:"
+  echo "https://sites.google.com/a/khanacademy.org/forge/for-developers/code-review-policy/using-phabricator/for-phabricator-admins"
+  exit 1
+fi
+
+# We'll need the right permissions file to push to production.
+# c.f. https://sites.google.com/a/khanacademy.org/forge/for-khan-employees/accessing-amazon-ec2-instances#TOC-Accessing-EC2-Instances
+if [ ! -s "$HOME/.ssh/internal_webserver.pem" ]; then
+  echo "You need to install internal_webserver.pem to push to production."
+  echo "At https://www.dropbox.com/home/Khan%20Academy%20All%20Staff/Secrets"
+  echo "download internal_webserver.pem and save it in your ~/.ssh directory"
+  exit 1
+fi
+
+
+# We always run from the same directory as where this script lives.
+# This is the only bash-ism in the script.
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# $1: the directory to cd to (root of some git tree).
+push_upstream() {
+(  cd "$@"
+   git checkout master
+   git pull
+   git pull upstream master
+   git push
+)
+}
+
+( cd phabricator/externals/javelin \
+  && git pull \
+  && cd .. \
+  && { git commit -a -m "update javelin" || true; } \
+)
+push_upstream phabricator
+push_upstream libphutil
+push_upstream arcanist
+
+# We don't just do 'git add .' since we want to make sure we update
+# the sub-repos properly.  (It's possible 'git add .' would work, but
+# this seems safer.)
+git status -s | sed -ne "s,^??.,,p" | xargs -r git add
+git status
+
+echo -n "Does everything look ok? (y/N) "
+read prompt
+if [ "$prompt" != "y" -a "$prompt" != "Y" -a "$prompt" != "yes" ]; then
+   echo "Aborting; user said no"
+   exit 1
+fi
+
+git commit -am "merge from upstream phabricator" && git push
+
+# Now push to production
+ssh ubuntu@phabricator.khanacademy.org -i "$HOME/.ssh/internal_webserver.pem" \
+   "cd internal-webserver; \
+    git pull; \
+    PHABRICATOR_ENV=khan phabricator/bin/phd stop; \
+    sudo /etc/init.d/lighttpd stop; \
+    PHABRICATOR_ENV=khan phabricator/bin/storage upgrade --force; \
+    sudo /etc/init.d/lighttpd start; \
+    PHABRICATOR_ENV=khan phabricator/bin/phd start; \
+   "
+
+echo "DONE!"
