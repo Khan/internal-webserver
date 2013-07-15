3254bf8a0d8d365165890e6f2674d6c30aac9586
Chris Klaiber <chris@khanacademy.org>
2013-07-15 11:13:56 -0700

    Explicit check for GNU xargs
    The -r flag is a GNU extension. On non-GNU systems the GNU tools are
    conventionally installed with a "g" prefix.
    
    Auditors: csilvers
    
    Test Plan:
    Fingers somewhat crossed. On Mac OS X, I ran "brew install findutils"
    and verified that man gxargs showed the -r option that I wanted.

--- update_phabricator.sh
+++ update_phabricator.sh
@@ -11,6 +11,14 @@ set -e
 # message when doing a 'git pull' from upstream.
 export GIT_MERGE_AUTOEDIT=no
 
+# We rely on a GNU extension to xargs, which may have a different name on
+# non-GNU systems like Mac OS X.
+if hash gxargs 2>/dev/null; then
+    XARGS=gxargs
+else
+    XARGS=xargs
+fi
+
 # We always run from the same directory as where this script lives.
 # This is the only bash-ism in the script.
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -52,7 +60,7 @@ push_upstream arcanist
 # We don't just do 'git add .' since we want to make sure we update
 # the sub-repos properly.  (It's possible 'git add .' would work, but
 # this seems safer.)
-git status -s | sed -ne "s,^??.,,p" | xargs -r git add
+git status -s | sed -ne "s,^??.,,p" | "$XARGS" -r git add
 git status
 
 echo -n "Does everything look ok? (y/N) "
