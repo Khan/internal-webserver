6b08faea5acc219062ef489a43b0c5bcf20bb626
Chris Klaiber <chris@khanacademy.org>
2014-07-21 18:08:50 -0700

    Update gae_util.fix_sys_path() to avoid conflict w/bigquery package
    When we installed the bigquery Python package to support querying BQ, we
    accidentally broke some dashboard-scraping cron scripts because the
    bigquery package is also under "google." and so import
    "google.appengine" is now looking at the wrong module!
    
    The failed (now fixed) traceback:
    
      Traceback (most recent call last):
        File "/home/ubuntu/internal-webserver/gae_dashboard/ka_report.py", line 31, in <module>
          import gae_dashboard_scrape
        File "/home/ubuntu/internal-webserver/gae_dashboard/gae_dashboard_scrape.py", line 33, in <module>
          import gae_dashboard_curl
        File "/home/ubuntu/internal-webserver/gae_dashboard/gae_dashboard_curl.py", line 32, in <module>
          from google.appengine.tools import appengine_rpc
      ImportError: No module named appengine.tools
    
    Auditors: csilvers
    
    Test Plan:
    I updated the code on toby and wasn't emailed an error message the next
    time the cron job ran.

--- gae_dashboard/gae_util.py
+++ gae_dashboard/gae_util.py
@@ -26,6 +26,7 @@ def _discover_sdk_path():
 
 def fix_sys_path(appengine_sdk_dir=None):
     """Update sys.path for appengine khan academy imports, also envvars."""
+    # This was originally copied  webapp/tools/appengine_tool_setup.py
 
     if 'SERVER_SOFTWARE' not in os.environ:
         os.environ['SERVER_SOFTWARE'] = 'Development'
@@ -35,6 +36,23 @@ def fix_sys_path(appengine_sdk_dir=None):
     if not appengine_sdk_dir:
         appengine_sdk_dir = _discover_sdk_path()
     sys.path.append(appengine_sdk_dir)
+
+    # We put this at the front of the path so 'import google' gets the
+    # appengine 'google' module, not another, un-useful google module
+    # that is installed as part of, say, the bigquery package.  In
+    # fact, bigquery is so annoying it registers python to import the
+    # (bad) google module at python-start time.  It uses pkgutil to do
+    # that, so we can update __path__ to include our path if needed.
+    for (module_name, module) in sys.modules.items():
+        if module_name == 'google' and hasattr(module, '__path__'):
+            # Fake appengine apps that expect 'google' to just be the
+            # appengine google.
+            module.__file__ = os.path.join(appengine_sdk_dir, 'google')
+            # Update the pkgutil __path__ var to say that google.foo
+            # imports should look in the appengine-sdk first.  cf.
+            # http://stackoverflow.com/questions/2699287/what-is-path-useful-for
+            module.__path__.insert(0, module.__file__)
+    sys.path.insert(0, appengine_sdk_dir)
 
     # Delegate the real work to the dev_appserver logic in the actual SDK.
     import dev_appserver
