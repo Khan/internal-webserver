3fc8cb625d80de37dde88d93a3a26a58f5a30ed8
Alexander Forsyth <alexanderforsyth@C02N4ALXG3QD.local>
2016-07-06 11:17:01 -0700

    Fetching GCE instance stats via cloud compute API and sending to stackdriver.
    fetch_instance_stats is a script to fetch GCE instance stats (currently just the number of failed instances) via the cloud compute API. These statistics are sent to Stackdriver. It is expected that this script will be run periodically via a cron job.
    
    Test Plan:
    fetch_instance_stats_test.py was run and passed. This tests some of the individual functions in fetch_instance_stats as well as the entire `main` function with the API calls mocked out.

--- gae_dashboard/fetch_instance_stats.py
+++ gae_dashboard/fetch_instance_stats.py
@@ -29,9 +29,11 @@ def _instance_is_failed(serial_port_output, unhealthy_count_threshold):
     """Return true if instance is considered failed.
 
     Return true if the gce instance is considered to have failed based
-    on there being at least unhealthy_count_threshold lines indicating
-    unhealthy instance status in the last 5*unhealthy_count_threshold
-    serial output port lines.
+    there are at least `unhealthy_count_threshold` lines indicating
+    unhealthy instance status in the last 5 * unhealthy_count_threshold
+    serial output port lines. The last `5 * x` lines was chosen because not
+    every output line is a health check. There can be other messages such as
+    heartbeats.
     """
     recent_lines = serial_port_output[-8 * unhealthy_count_threshold:]
     unhealthy_lines = filter(lambda l: 'STATUS=HEALTH_CHECK_UNHEALTHY' in l,
@@ -41,12 +43,17 @@ def _instance_is_failed(serial_port_output, unhealthy_count_threshold):
 
 def _get_serial_port_output_lines_from_cloud_compute(service, project_id,
                                                      gce_instance):
-    """Return a list of serial port output lines for the gce instance."""
+    """Return a list of serial port output lines for the gce instance.
+
+    List of serial port output lines for the gce instance is ordered from
+    least recent to most recent. With each port output as a separate list
+    entry."""
     request = service.instances().getSerialPortOutput(
         project=project_id, zone=gce_instance.zone_name,
         instance=gce_instance.instance_name)
     try:
         response = cloudmonitoring_util.execute_with_retries(request)
+    # This can fail, for example when an instance is spinning up.
     except apiclient.errors.HttpError as e:
         return []
     return response['contents'].split('\n')
@@ -80,16 +87,11 @@ def _get_instances_list_from_cloud_compute(service, project_id):
     return response
 
 
-def _send_num_failed_instances_to_cloud_monitoring(num_failed_instances,
-                                                   module_id):
-    metric_name = 'gce.number_failed_instances'
-    metric_labels = {'module_id': module_id}
-    alert = alertlib.Alert('Instance failure metrics')
-    alert.send_to_stackdriver(metric_name, metric_labels=metric_labels)
-
-
 def main(project_id, dry_run):
     service = cloudmonitoring_util.get_cloud_service('compute', 'v1')
+
+    # Get the number of failed GCE instances for each module of interest
+    # via the cloud compute API and put it into Stackdriver
     instance_list_response = _get_instances_list_from_cloud_compute(
         service, project_id)
 
@@ -112,8 +114,13 @@ def main(project_id, dry_run):
         if dry_run:
             print module_id, num_failed_instances
             continue
-        _send_num_failed_instances_to_cloud_monitoring(num_failed_instances,
-                                                       module_id)
+
+        # Send metric to Stackdriver
+        metric_name = 'gce.number_failed_instances'
+        metric_labels = {'module_id': module_id}
+        alert = alertlib.Alert('Instance failure metrics')
+        alert.send_to_stackdriver(metric_name, value=num_failed_instances,
+                                  metric_labels=metric_labels)
 
 if __name__ == '__main__':
     import argparse
---
+++ gae_dashboard/fetch_instance_stats_test.py
@@ -0,0 +1,126 @@
+import unittest
+
+import fetch_instance_stats
+
+
+class TestInstanceIsFailed(unittest.TestCase):
+    def test_unhealthy_over_threshold_returns_true(self):
+        serial_port_output = 'STATUS=HEALTH_CHECK_UNHEALTHY\n' * 10
+        serial_port_output_lines = serial_port_output.split('\n')
+        unhealthy_count_threshold = 5
+        self.assertTrue(fetch_instance_stats._instance_is_failed(
+            serial_port_output_lines, unhealthy_count_threshold))
+
+    def test_unhealthy_under_threshold_returns_false(self):
+        serial_port_output = 'STATUS=HEALTH_CHECK_UNHEALTHY\n' * 4
+        serial_port_output_lines = serial_port_output.split('\n')
+        unhealthy_count_threshold = 5
+        self.assertFalse(fetch_instance_stats._instance_is_failed(
+            serial_port_output_lines, unhealthy_count_threshold))
+
+    def test_not_unhealthy_returns_false(self):
+        serial_port_output = 'STATUS=HEALTH_CHECK_HEALTHY\n' * 10
+        serial_port_output_lines = serial_port_output.split('\n')
+        unhealthy_count_threshold = 5
+        self.assertFalse(fetch_instance_stats._instance_is_failed(
+            serial_port_output_lines, unhealthy_count_threshold))
+
+    def test_unhealthy_long_time_ago_returns_false(self):
+        serial_port_output = 'STATUS=HEALTH_CHECK_UNHEALTHY\n' * 10
+        serial_port_output += 'newline\n' * 100
+        serial_port_output_lines = serial_port_output.split('\n')
+        unhealthy_count_threshold = 5
+        self.assertFalse(fetch_instance_stats._instance_is_failed(
+            serial_port_output_lines, unhealthy_count_threshold))
+
+
+class TestGetInstancesFromResponse(unittest.TestCase):
+    def test_returns_only_instances_matching_name(self):
+        instance_list_response = {
+            'items':
+                {
+                    'zones/1': {
+                        'instances': [
+                            {'name': 'instance1'}, {'name': 'instance2'},
+                            {'name': 'e'}
+                        ]
+                    }
+                }
+        }
+        name_substring = 'instance'
+        instances = (fetch_instance_stats.
+                     _get_instances_matching_name_from_response(
+                         instance_list_response,
+                         name_substring))
+        self.assertEqual(2, len(instances))
+        self.assertTrue(all('instance' in instance.instance_name
+                        for instance in instances))
+
+
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.sent_to_cloud_monitoring = {}
+        self.mock_origs = {}   # used to unmock if needed
+
+        def new_send_to_stackdriver(alert, metric_name, value=1,
+                                    metric_labels=None):
+            module_id = metric_labels['module_id']
+            self.sent_to_cloud_monitoring[module_id] = (metric_name, value)
+
+        def new_get_instances_list_from_cloud_compute(service, project_id):
+            instance_list_response = {
+                'items':
+                    {
+                        'zones/1': {
+                            'instances': [
+                                {'name': 'gae-react--render-instance1'},
+                                {'name': 'gae-react--render-instance2'},
+                                {'name': 'err1'}, {'name': 'err2'}
+                            ]
+                        }
+                    }
+            }
+            return instance_list_response
+
+        def new_get_serial_port_output_lines_from_cloud_compute(service,
+                                                                project_id,
+                                                                gce_instance):
+            if 'instance' in gce_instance.instance_name:
+                serial_port_output = 'STATUS=HEALTH_CHECK_UNHEALTHY\n' * 10
+                serial_port_output_lines = serial_port_output.split('\n')
+                return serial_port_output_lines
+
+            serial_port_output = 'STATUS=HEALTH_CHECK_UNHEALTHY\n' * 2
+            serial_port_output_lines = serial_port_output.split('\n')
+            return serial_port_output_lines
+
+        self.mock(fetch_instance_stats.alertlib.Alert, 'send_to_stackdriver',
+                  new_send_to_stackdriver)
+        self.mock(fetch_instance_stats,
+                  '_get_instances_list_from_cloud_compute',
+                  new_get_instances_list_from_cloud_compute)
+        self.mock(fetch_instance_stats,
+                  '_get_serial_port_output_lines_from_cloud_compute',
+                  new_get_serial_port_output_lines_from_cloud_compute)
+
+    def mock(self, container, var_str, new_value):
+        if hasattr(container, var_str):
+            oldval = getattr(container, var_str)
+            self.mock_origs[(container, var_str)] = oldval
+            self.addCleanup(lambda: setattr(container, var_str, oldval))
+        else:
+            self.mock_origs[(container, var_str)] = None
+            self.addCleanup(lambda: delattr(container, var_str))
+        setattr(container, var_str, new_value)
+
+    def test_sent_errors_to_stackdriver(self):
+        """Mock out the appropriate functions, make a list of what would be
+        sent to Stackdriver"""
+
+        fetch_instance_stats.main('proj_id', False)
+        self.assertEqual({'react-render': ('gce.number_failed_instances', 2),
+                          'vm': ('gce.number_failed_instances', 0)},
+                         self.sent_to_cloud_monitoring)
+
+if __name__ == '__main__':
+    unittest.main()
