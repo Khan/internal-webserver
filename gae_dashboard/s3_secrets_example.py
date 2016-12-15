115c2210f0d88690be9c7650e2e58a8ecdb5885d
Nabil Hassein <nabil@khanacademy.org>
2016-12-15 18:42:49 -0500

    Rename gae_dashboard/secrets.py to s3_secrets.py to avoid name clash.
    Summary:
    fetch_instance_stats.py failed on toby because it tried to use this
    new secrets.py instead of the old one it should be using.
    This commit renames the new files to avoid any conflicts.
    
    Test Plan: observe logs
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    Differential Revision: https://phabricator.khanacademy.org/D32736

--- gae_dashboard/s3_secrets_example.py
+++ gae_dashboard/s3_secrets_example.py
@@ -1,4 +1,4 @@
 # These need to be copied from webapp's secrets.py into
-# gae_dashboard/secrets.py
+# gae_dashboard/s3_secrets.py
 youtube_export_s3_access_key = 'XXXX'
 youtube_export_s3_secret_key = 'XXXX'
