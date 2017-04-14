f6e05f55f5c381ccccda7753afd5fb5b07f7aa71
Colin Fuller <colin@khanacademy.org>
2017-04-14 10:45:22 -0400

    Lint ignore for khan-linter upgrade
    Summary:
    
    See D35059.  This change is the output of webapp's:
    `tools/add_lint_ignore_lines.py --todo_target colin ~/khan/internal-webserver`
    
    Also includes a manual pyflakes fix to prevent overwriting a variable via list
    comprehension and a substate of alertlib to pick up lint fixes
    
    Test Plan:
    `~/khan/devtools/khan-linter/runlint.py .`
    
    Auditors: csilvers

--- gae_dashboard/logs_bridge.py
+++ gae_dashboard/logs_bridge.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# TODO(colin): fix these lint errors (http://pep8.readthedocs.io/en/release-1.7.x/intro.html#error-codes)
+# pep8-disable:E127,E128
 
 """Send metrics extracted from App Engine logs to Cloud Monitoring.
 
--- update_phabricator_repositories.py
+++ update_phabricator_repositories.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# TODO(colin): fix these lint errors (http://pep8.readthedocs.io/en/release-1.7.x/intro.html#error-codes)
+# pep8-disable:E124
 
 """Update the phabricator webserver with the current list of repositories.
 
