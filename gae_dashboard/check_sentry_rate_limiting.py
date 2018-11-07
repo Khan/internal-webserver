cb6ec687cd7a59a345e358676877460a239522fd
Amos Latteier <amos@khanacademy.org>
2018-11-07 13:19:15 -0800

    Add an alert for sentry rate-limiting
    Summary:
    This is a simple script to notify us if Sentry
    starts rate-limiting the prod-js project. We
    want to know if this happens because it will make
    deploy-time monitoring not work correctly, and we'll
    want to adjust the Sentry config to reduce the
    error volume.
    
    I will also need to change the toby crontab in the
    aws-config repo to call this script once a day. That
    will be done in another diff.
    
    Also this script expects to have the Sentry API key
    placed in the sentry_api_key file. I'll have to do
    that manually on toby.
    
    Test Plan:
    - Run the script locally
    - Change the script to send an alert regardless of whether there is rate limiting and confirm that
      it sends to sentry.
    
    Reviewers: boris, andrew
    
    Differential Revision: https://phabricator.khanacademy.org/D50022

---
+++ gae_dashboard/check_sentry_rate_limiting.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""Check if Sentry is rate-limiting.
+
+We want to make sure that Sentry doesn't rate-limit the prod-js project. If it
+does that makes it hard to do good deploy-time monitoring since it may not be
+collecting errors during the deploy process.
+
+We've set up sentry to rate-limit other projects in hopes of keeping the total
+volume of errors low enough that prod-js doesn't get rate-limited.
+
+This script checks to see if the prod-js project has been rated limited in the
+past day and sends an alert if it has been.
+"""
+import os.path
+
+import alertlib
+import requests
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'sentry_api_key')) as f:
+    api_key = f.read().strip()
+
+
+def get_num_rejected():
+    "Return the number of events rate-limited in the last day"
+    resp = requests.get(
+        'https://sentry.io/api/0/projects/khanacademyorg/prod-js/stats/',
+        auth=(api_key, ''),
+        params={'resolution': '1d', 'stat': 'rejected'})
+    resp.raise_for_status()
+    data = resp.json()
+    return sum([n for (ts, n) in data])
+
+
+def main():
+    if get_num_rejected() == 0:
+        return
+    alertlib.Alert('Sentry prod-js project is being '
+                   'rate-limited').send_to_slack('#infrastructure-sre')
+
+
+if __name__ == '__main__':
+    main()
