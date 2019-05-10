6eff940b0cbabaf9434724bd2663c9724749fd7e
Alexander Shah <alexandershah@khanacademy.org>
2019-05-10 13:39:43 -0700

    Add query to check for scratchpad spam.
    Summary:
    Extension to dos detection script which scans for a large number of new scratchpads created from the same ip.
    
    Scratchpads are found using the bingo_conversion_events and are grouped by ip. Every ip with more than 50 (constant) scratchpads within the past 5 (constant) minutes will be collected and posted to slack. The ip is appended to db-ip.com for geographic lookup, and a sample user's profile is linked using their kaid for manual project checking.
    
    Rate limiting is already done (https://phabricator.khanacademy.org/D53866) but this change will hopefully make it easier examine details about the scratchpads (e.g. geographic location, looking at the projects themselves)
    
    Also did a bit of refactoring to the original dos script so that more constants could be shared with the scratchpad code and the ip sent through the slack message is linked in the same way.
    
    Test Plan:
    Ran script locally and checked that messages were posted in slack.
    Ran query in BigQuery console and checked that cost was cheap.
    
    {F1431171}
    
    {F1431172}
    
    ^ Modified the date information a little since there was no data for the past 5 minutes.
    
    Reviewers: amos, boris
    
    Reviewed By: amos
    
    Differential Revision: https://phabricator.khanacademy.org/D54291

--- gae_dashboard/dos_alert.py
+++ gae_dashboard/dos_alert.py
@@ -4,8 +4,8 @@
 This script does a very simplistic check for DoS attack and notifies us via
 slack if it notices anything that looks like a DoS attack.
 
-Note: we are only checking for the most simple types of attacks - a single
-client requesting the same URL repeatedly.
+Note: we are only checking for two simple types of attacks - a single
+client requesting the same URL repeatedly, and scratchpad spam.
 
 The hope is that this alerting will allow us to blacklist the offending IP
 address using the appengine firewall.
@@ -30,6 +30,11 @@ MAX_REQS_SEC = 4
 # The size of the period of time to query.
 PERIOD = 5 * 60
 
+TABLE_FORMAT = '%Y%m%d_%H'
+TS_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+ALERT_CHANNEL = '#infrastructure-sre'
+
 QUERY_TEMPLATE = """\
 SELECT
   ip,
@@ -40,24 +45,24 @@ FROM
   `logs_hourly.requestlogs_*`
 WHERE
   module_id NOT IN ( 'batch', 'react-render')
-  AND start_time_timestamp >= TIMESTAMP('{START_TIMESTAMP}')
-  AND start_time_timestamp < TIMESTAMP('{END_TIMESTAMP}')
+  AND start_time_timestamp >= TIMESTAMP('{start_timestamp}')
+  AND start_time_timestamp < TIMESTAMP('{end_timestamp}')
   AND STRPOS(resource, 'countBrandNewNotifications') = 0
   AND NOT STARTS_WITH(resource, '/_ah/')
-  AND _TABLE_SUFFIX BETWEEN '{START_TABLE}' and '{END_TABLE}'
+  AND _TABLE_SUFFIX BETWEEN '{start_table}' and '{end_table}'
 GROUP BY
   ip,
   resource,
   user_agent
 HAVING
-  count > {MAX_COUNT}
+  count > {max_count}
 ORDER BY
   count DESC
 """
 
 ALERT_TEMPLATE = """\
 *Possible DoS alert*
-IP: {ip}
+IP: <https://db-ip.com/{ip}|{ip}>
 Reqs in last 5 minutes: {count}
 URL: {resource}
 User agent: {user_agent}
@@ -67,25 +72,88 @@ Consider blacklisting IP using appengine firewall.
 <{log_link}|Stackdriver logs of reqs from IP>
 """
 
+SCRATCHPAD_QUERY_TEMPLATE = """\
+SELECT
+  ip,
+  ANY_VALUE(elog_user_kaid) as sample_kaid,
+  COUNT(bce.conversion) AS count
+FROM
+  `logs_hourly.requestlogs_*` kalog
+JOIN
+  kalog.bingo_conversion_events bce
+WHERE
+  bce.conversion = 'scratchpad_new_created'
+  AND start_time_timestamp >= TIMESTAMP('{start_timestamp}')
+  AND start_time_timestamp < TIMESTAMP('{end_timestamp}')
+  AND _TABLE_SUFFIX BETWEEN '{start_table}' and '{end_table}'
+GROUP BY
+  ip
+HAVING
+  count > {max_count}
+ORDER BY
+  count DESC
+"""
 
-def main():
-    end = datetime.datetime.utcnow()
-    start = end - datetime.timedelta(seconds=PERIOD)
-    table_format = '%Y%m%d_%H'
-    ts_format = '%Y-%m-%d %H:%M:%S'
+SCRATCHPAD_ALERT_INTRO_TEMPLATE = """\
+*Possible Scratchpad DoS alert*
+
+Below is a list of IPs which have submitted more than {max_count} new
+scratchpads in the last 5 minutes. A sample user's profile for each IP can be
+found through the links below.\n
+"""
+
+SCRATCHPAD_ALERT_ENTRY_TEMPLATE = """\
+IP: <https://db-ip.com/{ip}|{ip}>
+Count: {count}
+Sample: <https://www.khanacademy.org/profile/{sample_kaid}/projects|{sample_kaid}>
+"""
+
+# Alert if there are more than this many new scratchpads created by an IP in
+# the given period.
+MAX_SCRATCHPADS = 50
+
+
+def dos_detect(start, end):
     query = QUERY_TEMPLATE.format(
-        **{'START_TABLE': start.strftime(table_format),
-           'END_TABLE': end.strftime(table_format),
-           'START_TIMESTAMP': start.strftime(ts_format),
-           'END_TIMESTAMP': end.strftime(ts_format),
-           'MAX_COUNT': MAX_REQS_SEC * PERIOD})
+            start_table=start.strftime(TABLE_FORMAT),
+            end_table=end.strftime(TABLE_FORMAT),
+            start_timestamp=start.strftime(TS_FORMAT),
+            end_timestamp=end.strftime(TS_FORMAT),
+            max_count=(MAX_REQS_SEC * PERIOD))
     results = bq_util.call_bq(['query', '--nouse_legacy_sql', query],
                               project=BQ_PROJECT)
 
     for row in results:
         log_link = 'https://console.cloud.google.com/logs/viewer?project=khan-academy&folder=&organizationId=733120332093&minLogLevel=0&expandAll=false&advancedFilter=resource.type%3D%22gae_app%22%0AlogName%3D%22projects%2Fkhan-academy%2Flogs%2Fappengine.googleapis.com%252Frequest_log%22%0AprotoPayload.ip%3D%22{}%22'.format(row['ip'])
         msg = ALERT_TEMPLATE.format(log_link=log_link, **row)
-        alertlib.Alert(msg).send_to_slack('#infrastructure-sre')
+        alertlib.Alert(msg).send_to_slack(ALERT_CHANNEL)
+
+
+def scratchpad_detect(start, end):
+    scratchpad_query = SCRATCHPAD_QUERY_TEMPLATE.format(
+            start_table=start.strftime(TABLE_FORMAT),
+            end_table=end.strftime(TABLE_FORMAT),
+            start_timestamp=start.strftime(TS_FORMAT),
+            end_timestamp=end.strftime(TS_FORMAT),
+            max_count=MAX_SCRATCHPADS)
+
+    scratchpad_results = bq_util.call_bq(['query', '--nouse_legacy_sql',
+                                          scratchpad_query],
+                                         project=BQ_PROJECT)
+
+    if len(scratchpad_results) != 0:
+        msg = SCRATCHPAD_ALERT_INTRO_TEMPLATE.format(max_count=MAX_SCRATCHPADS)
+        msg += '\n'.join(SCRATCHPAD_ALERT_ENTRY_TEMPLATE.format(**row)
+                         for row in scratchpad_results)
+        alertlib.Alert(msg).send_to_slack(ALERT_CHANNEL)
+
+
+def main():
+    end = datetime.datetime.utcnow()
+    start = end - datetime.timedelta(seconds=PERIOD)
+
+    dos_detect(start, end)
+    scratchpad_detect(start, end)
 
 
 if __name__ == '__main__':
