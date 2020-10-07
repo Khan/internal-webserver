714445c6a0149463f0bc0c96e9e0cc5f4f9e1ceb
Craig Silverstein <csilvers@khanacademy.org>
2020-10-07 11:38:05 -0700

    Do the grouping of related builds manually, using GIT_REVISION.
    First, we use GIT_REVISION to group builds for a single deploy, rather
    than BUILDMASTER_DEPLOY_ID.  After talking to @benkraft, I decided
    this is the most reliable way to group jobs that are doing the same
    "deploy".  Using BUILDMASTER_DEPLOY_ID grouped multiple runs:
    deploying X, deploying X plus prior deploys, re-deploying X plus prior
    deploys as we move up in the queue, etc.  Each of those has a separate
    GIT_REVISION though, as we like.
    
    Second, I'm only interested right now in *completed* builds -- ones
    that go all the way through deploy-webapp.  So I now only retain
    grouped builds when the group contains a deploy-webapp job.  Because
    of that restriction, I no longer have download_jenkins_perf_data.py
    create the symlink-farm for grouped builds, but instead do it in our
    sh driver which can handle that more complex logic.
    
    Finally, I do a hacky thing to save some disk space: "delete" any
    .data files not associated with a completed build.  I put "delete" in
    scare quotes because we don't actually delete it, but instead replace
    it with a small marker-file, as a signal that it's already been
    processed.
    
    Issue: https://khanacademy.slack.com/archives/C8Y4Q1E0J/p1602091824108100
    
    Test plan:
    I copied this script manually to toby and ran it there, and looked at
    the symlink dirs and they all seemed reasonable.

--- download_jenkins_perf_data.sh
+++ download_jenkins_perf_data.sh
@@ -17,7 +17,48 @@ jenkins-perf-visualizer/download_jenkins_perf_data.py \
     deploy/webapp-test \
     deploy/e2e-test
 
+cd jenkins-perf-data
+
+# Now we want to collect build jobs that are part of the same deploy.
+# We join these builds based on GIT_REVISION; each deploy deploys a
+# unique git revision.  We only want to analyze "finished" deploys
+# so we start with the deploy-webapp job, and then find all associated
+# jobs.  We put them the related jobs into a s single symlink-farm
+# directory.
+#
+# TODO(csilvers): also include merge-branches jobs.  We'd need to fetch
+# the console logs for the merge-branches job, and look for the line:
+#    Resolved .* --> <GIT_REVISION>
+ls deploy--deploy-webapp*.data | while read dw; do
+    git_revision_text=$(grep -o '"GIT_REVISION": "[^"]*"' "$dw")
+    git_revision=$(echo "$git_revision_text" | cut -d'"' -f4)
+    symlink_farm=$(date -r "$dw" +"%Y-%m-%d.$git_revision")
+    if [ -d "$symlink_farm" ]; then
+        continue   # already processed this deploy-webapp file.
+    fi
+    mkdir "$symlink_farm"
+
+    # Find all jobs with the same GIT_REVISION.
+    grep -l "$git_revision_text" *.data | while read f; do
+        ln -s "../$f" "$symlink_farm/$f"
+    done
+done
+
+# I'm a bit worried about using up too much disk, so I do a hacky
+# thing to save some space: we can, conceptually, delete any .data
+# file that's not in one of the symlink-farm dirs from above; those
+# are builds that are not associated with a deploy.  But we can't
+# actually delete them, or they'll just be re-downloaded the next run
+# of this script.  Instead we just replace their content with
+# something small.
+find . -maxdepth 1 -name '*.data' -a -size +1k | while read f; do
+    if [ -z "$(find . -type l -name "$(basename "$f")")" ]; then
+        echo "Zeroing out $f: not a build used in a deploy"
+        echo "Not a deploy build" > "$f"
+    fi
+done
+
 # Get rid of files that are old, to bound the disk space we use.
-find jenkins-perf-data -type f -a -mtime +"$KEEP_DAYS" -print0 | xargs -0 rm || true
+find . -type f -a -mtime +"$KEEP_DAYS" -print0 | xargs -r -0 rm || true
 # Now get rid of an empty symlink-farm dirs.
-find jenkins-perf-data -depth -type d -print0 | xargs -0 rmdir 2>/dev/null || true
+find . -depth -type d -print0 | xargs -r -0 rmdir 2>/dev/null || true
