d05d5de2b3da7fad461842549f6130b55bfb780f
Craig Silverstein <csilvers@khanacademy.org>
2020-10-06 19:58:21 -0700

    Fix the paths on this script more.
    It's only like 3 lines long.  How can it have so many errors??
    
    Auditors: benkraft
    
    Test plan:
    Fingers crossed

--- download_jenkins_perf_data.sh
+++ download_jenkins_perf_data.sh
@@ -2,12 +2,14 @@
 
 # Download the latest performance data from Jenkins of all our deploys.
 # This also deletes old perf data files, so we don't use up all the disk.
+#
+# This should be run from ubuntu@toby:$HOME.
 
 # Delete all deploy-files that are over this many days old.
 KEEP_DAYS=30
 
-../jenkins-perf-visualizer/download_jenkins_perf_data.py \
-    --config="$HOME/internal-webserver/jenkins-perf-config.json" \
+jenkins-perf-visualizer/download_jenkins_perf_data.py \
+    --config=internal-webserver/jenkins-perf-config.json \
     deploy/merge-branches \
     deploy/2ndsmoketest-priming \
     deploy/build-webapp \
@@ -16,6 +18,6 @@ KEEP_DAYS=30
     deploy/e2e-test
 
 # Get rid of files that are old, to bound the disk space we use.
-find ../jenkins-perf-data -type f -a -mtime +"$KEEP_DAYS" -print0 | xargs -0 rm
+find jenkins-perf-data -type f -a -mtime +"$KEEP_DAYS" -print0 | xargs -0 rm
 # Now get rid of an empty symlink-farm dirs.
-find ../jenkins-perf-data -type d -print0 | xargs -0 rmdir 2>/dev/null
+find jenkins-perf-data -type d -print0 | xargs -0 rmdir 2>/dev/null
