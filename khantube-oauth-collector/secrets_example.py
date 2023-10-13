021aac97484fde8fb76947c691ef7212e14fb3d4
Craig Silverstein <csilvers@khanacademy.org>
2023-10-13 12:33:52 -0700

    [🔥AUDIT🔥] Document how to get secrets from GSM, not from secrets.py. (#50)
    🖍 _This is an audit!_ 🖍
    
    ## Summary:
    This way, we can remove the secrets from secrets.py, which doesn't
    otherwise need them.
    
    Issue: none
    
    ## Test plan:
    None
    
    Author: csilvers
    
    Auditors: #devops, #infra-platform
    
    Required Reviewers:
    
    Approved By:
    
    Checks:
    
    Pull Request URL: https://github.com/Khan/internal-webserver/pull/50

--- khantube-oauth-collector/secrets_example.py
+++ khantube-oauth-collector/secrets_example.py
@@ -1,4 +1,7 @@
-# These need to be copied from webapp's secrets.py into
-# khantube-oauth-collector/secrets.py
+# These need to be copied from GSM into khantube-oauth-collector/secrets.py
+# khantube_client_id is the "login" field at:
+#    https://console.cloud.google.com/security/secret-manager/secret/khantube_client/overview?project=khan-academy
+# khantube_client_secret is the secret at:
+#    https://console.cloud.google.com/security/secret-manager/secret/khantube_client/versions?project=khan-academy
 khantube_client_secret = 'XXXX'
 khantube_client_id = 'XXXX'
